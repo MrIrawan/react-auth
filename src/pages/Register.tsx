de3af4b44a2509bf2d6eead72a3ee15701eeeb90
farrel irawan <irawanssfarrel@gmail.com>
2025-03-05 00:04:14 +0700

    create register, login, and dashboard pages

---
+++ src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { supabase } from "../services/supabaseClient";
+import "../index.css";
+
+export default function Register() {
+    return (
+        <>
+        
+        </>
+    )
+}
